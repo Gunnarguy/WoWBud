2fcc1761ca009264fcebab3b66d6eafd2fce4c5a
gunnarguy <gunnarguy@me.com>
2025-04-30 08:30:08 -0700

    Initial Commit

---
+++ WoWBud/WoWBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoWBudApp.swift
+//  WoWBud
+//
+//  Created by Gunnar Hostetler on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WoWBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
